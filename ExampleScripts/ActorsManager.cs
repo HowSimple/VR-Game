b9c47036260df2a3324275b18b805636cd8c848b
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-09-12 23:18:25 -0500

    initial

---
+++ ExampleScripts/ActorsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class ActorsManager : MonoBehaviour
+    {
+        public List<Actor> Actors { get; private set; }
+        public GameObject Player { get; private set; }
+
+        public void SetPlayer(GameObject player) => Player = player;
+
+        void Awake()
+        {
+            Actors = new List<Actor>();
+        }
+    }
+
---
+++ ExampleScripts/Events.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+
+    // The Game Events used across the Game.
+    // Anytime there is a need for a new event, it should be added here.
+
+    public static class Events
+    {
+        
+        public static GameOverEvent GameOverEvent = new GameOverEvent();
+        public static PlayerDeathEvent PlayerDeathEvent = new PlayerDeathEvent();
+        public static EnemyKillEvent EnemyKillEvent = new EnemyKillEvent();
+        public static PickupEvent PickupEvent = new PickupEvent();
+     
+        public static DamageEvent DamageEvent = new DamageEvent();
+        public static DisplayMessageEvent DisplayMessageEvent = new DisplayMessageEvent();
+    }
+
+   
+
+    public class GameOverEvent : GameEvent
+    {
+        public bool Win;
+    }
+
+    public class PlayerDeathEvent : GameEvent { }
+
+    public class EnemyKillEvent : GameEvent
+    {
+        public GameObject Enemy;
+        public int RemainingEnemyCount;
+    }
+
+    public class PickupEvent : GameEvent
+    {
+        public GameObject Pickup;
+    }
+
+
+
+    public class DamageEvent : GameEvent
+    {
+        public GameObject Sender;
+        public float DamageValue;
+    }
+
+    public class DisplayMessageEvent : GameEvent
+    {
+        public string Message;
+        public float DelayBeforeDisplay;
+    }
+
