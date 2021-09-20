24ed243a32ae9436c910e3f783611a9e42fd1ffd
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-09-19 23:52:00 -0500

    Working gun muzzle flash and crosshair

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -6,7 +6,7 @@ public class Gun : MonoBehaviour {
     public float range = 100f;
 
     public Camera fpsCam;
-
+    public ParticleSystem muzzleFlash;
     void Update() {
         if (Input.GetButtonDown("Fire"))
         {
@@ -15,6 +15,7 @@ public class Gun : MonoBehaviour {
     }
     void Shoot() {
         RaycastHit hit;
+        muzzleFlash.Play();
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
