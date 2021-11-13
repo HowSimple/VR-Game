ece46057755a7232ff85809cd8848af3f7e4d0bc
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-11-13 12:59:52 -0600

    Health script applies player death

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -6,17 +6,40 @@ public class Health : MonoBehaviour
 {
 
     public float healthPoints = 50f;
+    public bool healthRegenEnabled = false;
+    public float regenRate;
+    public float regenDelay;
 
+
+    void enableRegen()
+    {
+        healthRegenEnabled = true;
+    }
+    public void FixedUpdate()
+    {
+        if (healthRegenEnabled)
+            healthPoints += regenRate * Time.deltaTime;
+
+    }
+    public void PlayerDeath()
+    {
+
+    }
     public void takeDamage(float amount)
     {
         healthPoints -= amount;
         if (healthPoints <= 0f)
         {
-            Die();
+           if(gameObject.name == "Player")
+                PlayerDeath();
+           else Die();
+
         }
 
-    }
 
+
+    }
+    
     void Die()
     {
         Destroy(gameObject);
