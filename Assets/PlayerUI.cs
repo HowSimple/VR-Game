b7a999a13dbc60147c352d5d4b54c667e27fa0bb
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-11-19 15:15:58 -0600

    added UI class

---
+++ Assets/PlayerUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UI;
+public class PlayerUI : MonoBehaviour
+{
+    private Health hp;
+    private AudioSource audio;
+    public TMP_Text healthUI;
+    //public AudioClip switchSoundEffect;
+    public Image jetRechargeBar;
+    private float jetCharge;
+
+    private void Awake()
+    {
+       
+        
+        //dash = this.GetComponent<Dash>();
+        hp = this.GetComponent<Health>();
+      
+        //jetRechargeBar.fillAmount= (100);
+    }
+    private void FixedUpdate()
+    {
+        jetCharge = this.GetComponent<PlayerMovement>().jetCharge;
+        jetRechargeBar.fillAmount= jetCharge;
+        healthUI.text = "Health: "  +hp.healthPoints.ToString();
+
+    }
+
+}
