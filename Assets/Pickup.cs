e3122499c733c06d760cd13eb15721e1e9ff2a7b
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-11-21 17:44:03 -0600

    Create Pickup.cs

---
+++ Assets/Pickup.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pickup : MonoBehaviour
+{
+   //private Health hp;
+   void Start (){
+     //  hp = GetComponent<Health>();
+   }
+}
